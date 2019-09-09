7b4d60c1d5fe96c7a2f9dabff2ccd0197361f74c
Matheus Duarte <matheusduartegalvao@gmail.com>
2019-09-09 17:28:00 -0300

    Adicao de evento de clique nas imagens do anuncio

--- app/src/main/java/com/duarte/olxapp/activity/CadastrarAnuncioActivity.java
+++ app/src/main/java/com/duarte/olxapp/activity/CadastrarAnuncioActivity.java
@@ -1,27 +1,51 @@
 package com.duarte.olxapp.activity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.blackcat.currencyedittext.CurrencyEditText;
 import com.duarte.olxapp.R;
+import com.duarte.olxapp.helper.Permissoes;
 import com.santalu.maskedittext.MaskEditText;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
-public class CadastrarAnuncioActivity extends AppCompatActivity {
+public class CadastrarAnuncioActivity extends AppCompatActivity implements View.OnClickListener{
 
     private EditText campoTitulo, campoDescricao;
+    private ImageView imagem1, imagem2, imagem3;
     private MaskEditText campoTelefone;
     private CurrencyEditText campoValor;
+
+    private String[] permissoes = new String[]{
+            Manifest.permission.READ_EXTERNAL_STORAGE
+    };
+
+    private List<String> listaFotosRecuperadas = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_anuncio);
+
+        //Validar permissoes
+        Permissoes.validarPermissoes(permissoes, this, 1);
 
         inicializarComponentes();
     }
@@ -33,16 +57,96 @@ public class CadastrarAnuncioActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onClick(View v) {
+
+        switch ( v.getId() ){
+            case R.id.imageCadastro1 :
+                escolherImagem(1);
+                break;
+            case R.id.imageCadastro2 :
+                escolherImagem(2);
+                break;
+            case R.id.imageCadastro3 :
+                escolherImagem(3);
+                break;
+        }
+    }
+
+    public void escolherImagem(int requestCode){
+        Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, requestCode);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if( resultCode == Activity.RESULT_OK){
+
+            //Recuperar Imagem
+            Uri imagemSelecionada = data.getData();
+            String caminhoImagem = imagemSelecionada.toString();
+
+            //Configura imagem no imageView
+            if( requestCode == 1 ){
+                imagem1.setImageURI( imagemSelecionada );
+            }else if( requestCode == 2 ){
+                imagem2.setImageURI( imagemSelecionada );
+            }else if( requestCode == 3 ){
+                imagem3.setImageURI( imagemSelecionada );
+            }
+
+            listaFotosRecuperadas.add( caminhoImagem );
+        }
+    }
+
     private void inicializarComponentes(){
 
         campoTitulo = findViewById(R.id.editTitulo);
         campoTelefone = findViewById(R.id.editTelefone);
         campoDescricao = findViewById(R.id.editDescricao);
         campoValor = findViewById(R.id.editTValor);
+        imagem1 = findViewById(R.id.imageCadastro1);
+        imagem2 = findViewById(R.id.imageCadastro2);
+        imagem3 = findViewById(R.id.imageCadastro3);
+        imagem1.setOnClickListener(this);
+        imagem2.setOnClickListener(this);
+        imagem3.setOnClickListener(this);
 
         //Configura a localidade para pt -> português BR -> Brasil
         Locale locale = new Locale("pt", "BR");
         campoValor.setLocale( locale );
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        for( int permissaoResultado : grantResults ){
+            if( permissaoResultado == PackageManager.PERMISSION_DENIED){
+                alertaValidacaoPermissao();
+            }
+        }
+    }
+
+    private void alertaValidacaoPermissao(){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissões negadas");
+        builder.setMessage("Para utilizar o app é necessário aceitar as permissões");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
 
     }
 }
