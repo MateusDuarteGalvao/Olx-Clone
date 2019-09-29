30c235383ce711ef64252d906f07f12dfd5c599e
Matheus Duarte <matheusduartegalvao@gmail.com>
2019-09-29 16:32:41 -0300

    Ajustes na interface de anuncios

--- app/src/main/java/com/duarte/olxapp/activity/MeusAnunciosActivity.java
+++ app/src/main/java/com/duarte/olxapp/activity/MeusAnunciosActivity.java
@@ -5,17 +5,43 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.duarte.olxapp.R;
+import com.duarte.olxapp.adapter.AdapterAnuncios;
+import com.duarte.olxapp.helper.ConfiguracaoFirebase;
+import com.duarte.olxapp.model.Anuncio;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MeusAnunciosActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerAnuncios;
+    private List<Anuncio> anuncios = new ArrayList<>();
+    private AdapterAnuncios adapterAnuncios;
+    private DatabaseReference anuncioUsuarioRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meus_anuncios);
+
+        //Configurações inicais
+        anuncioUsuarioRef = ConfiguracaoFirebase.getFirebase()
+                .child("meus_anuncios")
+                .child( ConfiguracaoFirebase.getIdUsuario() );
+
+        incializarComponentes();
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -27,6 +53,45 @@ public class MeusAnunciosActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Configurar RecyclerView
+        recyclerAnuncios.setLayoutManager(new LinearLayoutManager(this));
+        recyclerAnuncios.setHasFixedSize(true);
+        adapterAnuncios = new AdapterAnuncios(anuncios, this);
+
+        recyclerAnuncios.setAdapter(adapterAnuncios);
+
+        //Recupera os anuncios para o usuário
+        recuperarAnuncios();
+    }
+
+    private void recuperarAnuncios() {
+
+        anuncioUsuarioRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                anuncios.clear();
+                for ( DataSnapshot ds : dataSnapshot.getChildren() ) {
+                    anuncios.add( ds.getValue(Anuncio.class) );
+                }
+
+                Collections.reverse( anuncios );
+                adapterAnuncios.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    private void incializarComponentes() {
+
+        recyclerAnuncios = findViewById(R.id.recyclerAnuncios);
     }
 
 }
